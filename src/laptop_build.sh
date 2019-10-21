c03910e93a2df14152f381d1f9ffb83ea5862fcd
adrian <94568a@gmail.com>
2019-10-21 12:45:22 -0700

    rewrote build script
    cross compiler on my laptop is broken

---
+++ src/laptop_build.sh
@@ -0,0 +1,47 @@
+# Set up path and other vars.
+export PATH=$PATH:/opt/cross/bin
+export PATH=$PATH:/mnt/c/buildto/bin
+
+# Assemble bootsector and write to first sector of disk.
+nasm -f bin ./bootloader/bootsector.asm -o ../build/bootsector.bin
+dd if=../build/bootsector.bin of=../img/disk.img
+
+# Assemble 1and write bootloader.
+nasm -f bin ./bootloader/bootloader.asm -o ../build/bootloader.bin
+dd if=../build/bootloader.bin of=../img/disk.img seek=1 bs=512
+
+# Assemble protected mode stuff.
+nasm -f elf ./kernel/pmbootloader.asm -o ../build/pmbootloader.o
+nasm -f elf ./kernel/misc.asm -o ../build/misc_asm.o
+nasm -f elf ./kernel/idt.asm -o ../build/idt_asm.o
+nasm -f elf ./kernel/exception.asm -o ../build/exception_asm.o
+nasm -f elf ./kernel/gdt.asm -o ../build/gdt_asm.o
+nasm -f elf ./kernel/irq.asm -o ../build/irq_asm.o
+nasm -f elf ./kernel/paging.asm -o ../build/paging_asm.o
+
+export CFLAGS="-B/mnt/c/buildto/bin/i686-elf- -std=gnu99 -ffreestanding -Wall -Wextra -g"
+
+# Compile kernel (C).
+i686-elf-gcc -c ./kernel/kernel.c -o ../build/kernel.o $CFLAGS
+i686-elf-gcc -c ./kernel/textmode.c -o ../build/textmode.o $CFLAGS
+i686-elf-gcc -c ./kernel/gdt.c -o ../build/gdt.o $CFLAGS
+i686-elf-gcc -c ./kernel/idt.c -o ../build/idt.o $CFLAGS
+i686-elf-gcc -c ./kernel/misc.c -o ../build/misc.o $CFLAGS
+i686-elf-gcc -c ./kernel/exception.c -o ../build/exception.o $CFLAGS
+i686-elf-gcc -c ./kernel/memorymap.c -o ../build/memorymap.o $CFLAGS
+i686-elf-gcc -c ./kernel/pic.c -o ../build/pic.o $CFLAGS
+i686-elf-gcc -c ./kernel/irq.c -o ../build/irq.o $CFLAGS
+i686-elf-gcc -c ./kernel/acpi.c -o ../build/acpi.o $CFLAGS
+i686-elf-gcc -c ./kernel/hpet.c -o ../build/hpet.o $CFLAGS
+i686-elf-gcc -c ./kernel/pit.c -o ../build/pit.o $CFLAGS
+i686-elf-gcc -c ./kernel/paging.c -o ../build/paging.o $CFLAGS
+i686-elf-gcc -c ./kernel/serial.c -o ../build/serial.o $CFLAGS
+
+# Link kernel binary.
+i686-elf-gcc -T ./kernel/linker.ld -o ../build/kernel.bin -ffreestanding -O0 -nostdlib ../build/pmbootloader.o ../build/kernel.o ../build/misc.o ../build/misc_asm.o ../build/textmode.o ../build/idt.o ../build/idt_asm.o ../build/exception.o ../build/exception_asm.o ../build/gdt_asm.o ../build/gdt.o ../build/memorymap.o ../build/irq_asm.o ../build/irq.o ../build/pic.o ../build/acpi.o ../build/hpet.o ../build/paging.o ../build/paging_asm.o ../build/serial.o  
+
+# Write kernel binary.
+dd if=../build/kernel.bin of=../img/disk.img seek=5 bs=512
+
+# Make a copy of the disk so it can be viewed while QEMU is running.
+cp ../img/disk.img ../img/diskcopy.img
